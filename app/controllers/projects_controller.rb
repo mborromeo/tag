235bcdbd7a8144a02bc48e556d71389390eb668f
Marco Borromeo <marco@marco-borromeo.com>
2008-11-16 16:41:10 +0100

    dropdown for parent

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -36,6 +36,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1/edit
   def edit
     @project = Project.find(params[:id])
+    @projects = Project.find(:all)
   end
 
   # POST /projects
