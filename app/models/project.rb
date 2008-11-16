77692ca517c9175613eff107db4ce555a0f1a70c
Chiaroscuro <Kia@ivans-macbook-air.local>
2008-11-16 16:58:41 +0100

    manage null parent for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ActiveRecord::Base
+  
+  def parent_name
+    return "no parent" unless self.parent
+    project = Project.find(self.parent)
+    project ? project.name : "no parent"
+  end
 end
